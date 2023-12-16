1adf795fb8a4f246ddc34d34093ff41f6cfee371
bobby096310 <chiuchienhao@outlook.com>
2023-12-16 18:30:01 +0100

    remove be and init the component

---
+++ landing/src/Component/Landing.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import "./Landing.css"
+
+const Landing = () => {
+    return (
+        <>
+            <div className="header">
+                <div className="logo"></div>
+            </div>
+            <div className="banner">
+                {/*<h1>This is our Landing Page!</h1>*/}
+                {/*<div className="image"><img src="highway_bg.jpg" alt="background"/></div>*/}
+                <div className="info"></div>
+            </div>
+        </>
+    );
+}
+
+export default Landing;
