e429b3c852faad1e038c90365dbb2680476faa56
bobby096310 <chiuchienhao@outlook.com>
2023-12-16 22:08:49 +0100

    adjust button positions

--- src/Component/Landing.tsx
+++ src/Component/Landing.tsx
@@ -8,20 +8,27 @@ const Landing = () => {
         <>
             <div className="header">
                 <div className="logo"></div>
+                <div className="buttons">
+                    <div className="btn">
+                        <Button id="book" variant="outlined">Book a Test</Button>
+                    </div>
+                    <div className="btn">
+                        <Button id="contact" variant="contained">Contact Sales</Button>
+                    </div>
+                </div>
             </div>
             <div className="banner">
                 <div className="info-container">
                     <div className="info">
-                        <div className="buttons">
-                            <Button variant="contained">Book a Test</Button>
-                            <br/>
-                            <Button variant="contained">Contact Sales</Button>
-                        </div>
+                        {/*<div className="buttons">*/}
+                        {/*    <Button style={{"border": "1px solid lightgreen","backgroundColor": "black", "color":"lightgreen"}} variant="outlined">Book a Test</Button>*/}
+                        {/*    <Button style={{"border": "1px solid lightgreen","backgroundColor": "black", "color":"lightgreen"}} variant="contained">Contact Sales</Button>*/}
+                        {/*</div>*/}
                     </div>
                 </div>
             </div>
             <div className="services">
-                <Service image="talk.png" text="Technology Consulting"/>
+                <Service image="talk.png" text="Tech Consulting"/>
                 <Service image="driving-school.png" text="Testing"/>
                 <Service image="vital-signs.png" text="Simulation Center"/>
             </div>
